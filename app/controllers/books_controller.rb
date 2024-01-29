c65f9ce443833ce83497f78880d480f9503506d9
Souax <126418531+Souax@users.noreply.github.com>
2024-01-29 06:17:31 +0000

    完成

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,59 @@
+class BooksController < ApplicationController
+  def index
+    @user = current_user
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    else
+      @user = current_user
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def show
+    @book = Book.new
+    @books = Book.find(params[:id])
+    @user = @books.user
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+    @user = @book.user
+    unless @user.id == current_user.id
+      redirect_to books_path
+    end
+  end
+
+def update
+    @book = Book.find(params[:id])
+  if @book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(current_user)
+  else
+      render :edit
+  end
+end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+
+   private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+
+
+end
